6710abc735672402f3ec4a218d0bbf545aca7e65
Vedant Dalvi <vgdalvi1997@gmail.com>
2022-07-03 16:19:15 +0200

    ACTIONS

--- Raspberry_pi/Actuators.py
+++ Raspberry_pi/Actuators.py
@@ -1,18 +1,20 @@
-import time
+from __future__ import print_function
 from datetime import datetime
-import paho.mqtt.client as paho
 import json
+import random
+import time
 import RPi.GPIO as GPIO
 from grovepi import *
 
+
+import sys
+import time
+
+from relay_lib_seeed import *
+
+
 ultra_1 = 4
 ultra_2 = 6
-
-percentage = 0
-biodist = 0
-biobin = 0
-nonbiodist = 0
-nonbiobin = 0
 a = 0
 angle = 90
 percentage = 0
@@ -22,39 +24,56 @@ GPIO.setup(11,GPIO.OUT)   # Set pin 11 as an output, and define as servo1 as PWM
 servo1 = GPIO.PWM(11,50) # pin 11 for servo1, pulse 50Hz
 servo1.start(0)         # Start PWM running, with value of 0 (pulse off)
 
-def setangle(angle):
+def process_loop():
+    # turn all of the relays on
+    relay_all_on()
+    # wait a second
+    time.sleep(15)
+    # turn all of the relays off
+    relay_all_off()
+    # wait a second
+    time.sleep(1)
+
+def SetAngle(angle):
    servo1.ChangeDutyCycle(2+(angle/18))
-   time.sleep(10)
+   time.sleep(5)
    servo1.ChangeDutyCycle(0)  
             
-def US_S1(dist_1):
-    print(dist_1,'cm')
 		 	
-def US_bin_selection(dist, percentage):                                #Read distance value from Ultrasonic
+def US_bin_depth(dist, percentage):   
+    dist = ultrasonicRead(ultra_2)                             #Read distance value from Ultrasonic
     print("Bin Distance=" , dist, "cm")
     percentage = 100 * (6 - dist)/6
     print("Bin Status=" , percentage, "%")
     if percentage >= 80:
 	    print("Bin is 80% full, Please change the Bin!")
+     
+                                          
 
-def on_connect(client, userdata, flags, rc):                # func for making connection
-    print("Connection returned result: " + str(rc) )
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("Actions" , 1 )                       # Subscribe to "Actions" topic
+try:
+    while True:
+        dist_1 = ultrasonicRead(ultra_1)
+	print("Distance",dist_1)
+        if dist_1 <= 4:
+            relay_on(1,2)
+            time.sleep(10) #see after running conveyor
+            relay_off(1,2)
 
-def on_message(client, userdata, msg):                      # Func for receiving msgs
-    act = json.loads(str(msg.payload.decode("utf-8")))
-    for key, value in act.items():
-        if key == "Fan":
-            fan(value)
-        if key == "Servo":
-            servo(value)
-        if key == "LED":
-            led(value)        
+            a = random.randint(0,9)
+            print(a)
+            if a%2 == 0:
+                angle = 0
+                SetAngle(angle) #bio
     
-mqttc = paho.Client()                                       # mqttc object
-mqttc.on_connect = on_connect                               # assign on_connect func
-mqttc.on_message = on_message                               # assign on_message func
-mqttc.connect("192.168.0.145", 1883, keepalive=60)          # connect to pi
-mqttc.loop_start()                                          # Start receiving in loop
+            else:
+                angle = 180
+                SetAngle(angle)  #non_bio
+
+
+        
+
+finally:
+    #Clean things up at the end
+    servo1.stop()
+    GPIO.cleanup()
+    print("Goodbye!")
